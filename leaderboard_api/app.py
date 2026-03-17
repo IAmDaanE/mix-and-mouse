84c882ce682d18f765597964e9742943546ae5cb
Daan Eeckloo <daan.eeckloo@gmail.com>
2026-03-17 19:42:31 +0100

    basic api structure for testing

--- leaderboard_api/app.py
+++ leaderboard_api/app.py
@@ -1,2 +1,14 @@
-#this is for the api code being deployed
-print("Hello World")
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+database_url = psycopg2.connect(os.getenv("DATABASE_URL"))
+
+@app.route("/ping")
+def ping():
+    return "succesful response"
+
+@app.route("/database_url")
+def display_url():
+    return str(database_url)
