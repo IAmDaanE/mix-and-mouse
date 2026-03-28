86d6f79638789422484a7b747171180f29fc8f0b
Twan <twanVanhoutte@gmail.com>
2026-03-28 15:24:40 +0100

    refacture by seperating recipes from main

---
+++ recipes.py
@@ -0,0 +1,368 @@
+all_recipes_in_game = [
+        {"name": "mojito",
+        "price": 8,
+        "makingprocess": {
+            "ing1": {"name": "white rum", "amount": 6},
+            "ing2": {"name": "lime juice", "amount": 4},
+            "ing3": {"name": "sugar syrup", "amount": 2},
+            "ing4": {"name": "soda water", "amount": 6},
+            "ing5": {"name": "mint", "amount": 2},
+            "ing6": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "cosmopolitan",
+        "price": 11,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 8},
+            "ing2": {"name": "triple sec", "amount": 4},
+            "ing3": {"name": "cranberry juice", "amount": 6},
+            "ing4": {"name": "lime juice", "amount": 2}
+        }},
+
+        {"name": "margarita",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "tequila", "amount": 10},
+            "ing2": {"name": "triple sec", "amount": 4},
+            "ing3": {"name": "lime juice", "amount": 6},
+            "ing4": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "piña colada",
+        "price": 12,
+        "makingprocess": {
+            "ing1": {"name": "white rum", "amount": 6},
+            "ing2": {"name": "pineapple juice", "amount": 8},
+            "ing3": {"name": "coconut cream", "amount": 6},
+            "ing4": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "whiskey sour",
+        "price": 9,
+        "makingprocess": {
+            "ing1": {"name": "whiskey", "amount": 9},
+            "ing2": {"name": "lemon juice", "amount": 6},
+            "ing3": {"name": "sugar syrup", "amount": 3},
+            "ing4": {"name": "egg white", "amount": 2},
+            "ing5": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "daiquiri",
+        "price": 8,
+        "makingprocess": {
+            "ing1": {"name": "white rum", "amount": 10},
+            "ing2": {"name": "lime juice", "amount": 6},
+            "ing3": {"name": "sugar syrup", "amount": 4}
+        }},
+
+        {"name": "long island iced tea",
+        "price": 14,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 2},
+            "ing2": {"name": "gin", "amount": 2},
+            "ing3": {"name": "white rum", "amount": 2},
+            "ing4": {"name": "tequila", "amount": 2},
+            "ing5": {"name": "triple sec", "amount": 2},
+            "ing6": {"name": "lemon juice", "amount": 4},
+            "ing7": {"name": "cola", "amount": 6},
+            "ing8": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "tequila sunrise",
+        "price": 9,
+        "makingprocess": {
+            "ing1": {"name": "tequila", "amount": 6},
+            "ing2": {"name": "orange juice", "amount": 12},
+            "ing3": {"name": "grenadine", "amount": 2},
+            "ing4": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "screwdriver",
+        "price": 7,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 6},
+            "ing2": {"name": "orange juice", "amount": 14},
+            "ing3": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "moscow mule",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 6},
+            "ing2": {"name": "ginger beer", "amount": 12},
+            "ing3": {"name": "lime juice", "amount": 2},
+            "ing4": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "dark & stormy",
+        "price": 11,
+        "makingprocess": {
+            "ing1": {"name": "dark rum", "amount": 8},
+            "ing2": {"name": "ginger beer", "amount": 10},
+            "ing3": {"name": "lime juice", "amount": 2},
+            "ing4": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "amaretto sour",
+        "price": 9,
+        "makingprocess": {
+            "ing1": {"name": "amaretto", "amount": 9},
+            "ing2": {"name": "lemon juice", "amount": 6},
+            "ing3": {"name": "sugar syrup", "amount": 3},
+            "ing4": {"name": "egg white", "amount": 2},
+            "ing5": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "sex on the beach",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 6},
+            "ing2": {"name": "cranberry juice", "amount": 6},
+            "ing3": {"name": "orange juice", "amount": 6},
+            "ing4": {"name": "grenadine", "amount": 2},
+            "ing5": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "gimlet",
+        "price": 8,
+        "makingprocess": {
+            "ing1": {"name": "gin", "amount": 12},
+            "ing2": {"name": "lime juice", "amount": 6},
+            "ing3": {"name": "sugar syrup", "amount": 2}
+        }},
+
+        {"name": "espresso martini",
+        "price": 13,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 8},
+            "ing2": {"name": "kahlúa", "amount": 8},
+            "ing3": {"name": "cream", "amount": 4}
+        }},
+
+        {"name": "white russian",
+        "price": 12,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 8},
+            "ing2": {"name": "kahlúa", "amount": 6},
+            "ing3": {"name": "cream", "amount": 6},
+            "ing4": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "porn star martini",
+        "price": 15,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 7},
+            "ing2": {"name": "amaretto", "amount": 3},
+            "ing3": {"name": "pineapple juice", "amount": 5},
+            "ing4": {"name": "lime juice", "amount": 2},
+            "ing5": {"name": "champagne", "amount": 3}
+        }},
+
+        {"name": "gin & tonic",
+        "price": 7,
+        "makingprocess": {
+            "ing1": {"name": "gin", "amount": 6},
+            "ing2": {"name": "tonic water", "amount": 12},
+            "ing3": {"name": "lime", "amount": 2},
+            "ing4": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "cuba libre",
+        "price": 8,
+        "makingprocess": {
+            "ing1": {"name": "white rum", "amount": 6},
+            "ing2": {"name": "cola", "amount": 12},
+            "ing3": {"name": "lime juice", "amount": 2},
+            "ing4": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "french 75",
+        "price": 13,
+        "makingprocess": {
+            "ing1": {"name": "gin", "amount": 6},
+            "ing2": {"name": "lemon juice", "amount": 4},
+            "ing3": {"name": "sugar syrup", "amount": 2},
+            "ing4": {"name": "champagne", "amount": 8}
+        }},
+
+        {"name": "bellini prosecco",
+        "price": 12,
+        "makingprocess": {
+            "ing1": {"name": "prosecco", "amount": 14},
+            "ing2": {"name": "peach juice", "amount": 6}
+        }},
+
+        {"name": "peach fizz",
+        "price": 9,
+        "makingprocess": {
+            "ing1": {"name": "peach schnapps", "amount": 6},
+            "ing2": {"name": "orange juice", "amount": 8},
+            "ing3": {"name": "soda water", "amount": 6},
+            "ing4": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "blue lagoon",
+        "price": 11,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 6},
+            "ing2": {"name": "blue curaçao", "amount": 4},
+            "ing3": {"name": "lemon juice", "amount": 2},
+            "ing4": {"name": "soda water", "amount": 8},
+            "ing5": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "blue margarita",
+        "price": 12,
+        "makingprocess": {
+            "ing1": {"name": "tequila", "amount": 10},
+            "ing2": {"name": "blue curaçao", "amount": 4},
+            "ing3": {"name": "lime juice", "amount": 6},
+            "ing4": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "midori sour",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "midori", "amount": 9},
+            "ing2": {"name": "lemon juice", "amount": 6},
+            "ing3": {"name": "sugar syrup", "amount": 3},
+            "ing4": {"name": "egg white", "amount": 2},
+            "ing5": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "japanese slipper",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "midori", "amount": 8},
+            "ing2": {"name": "triple sec", "amount": 8},
+            "ing3": {"name": "lemon juice", "amount": 4}
+        }},
+
+        {"name": "death in the afternoon",
+        "price": 15,
+        "makingprocess": {
+            "ing1": {"name": "absinthe", "amount": 4},
+            "ing2": {"name": "champagne", "amount": 16}
+        }},
+
+        {"name": "corpse reviver",
+        "price": 13,
+        "makingprocess": {
+            "ing1": {"name": "gin", "amount": 7},
+            "ing2": {"name": "triple sec", "amount": 5},
+            "ing3": {"name": "absinthe", "amount": 1},
+            "ing4": {"name": "lemon juice", "amount": 7}
+        }},
+
+        {"name": "negroni",
+        "price": 12,
+        "makingprocess": {
+            "ing1": {"name": "gin", "amount": 7},
+            "ing2": {"name": "campari", "amount": 7},
+            "ing3": {"name": "sweet vermouth", "amount": 6},
+            "ing4": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "americano",
+        "price": 11,
+        "makingprocess": {
+            "ing1": {"name": "campari", "amount": 7},
+            "ing2": {"name": "sweet vermouth", "amount": 7},
+            "ing3": {"name": "soda water", "amount": 6},
+            "ing4": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "dry martini",
+        "price": 14,
+        "makingprocess": {
+            "ing1": {"name": "gin", "amount": 16},
+            "ing2": {"name": "dry vermouth", "amount": 4}
+        }},
+
+        {"name": "manhattan",
+        "price": 13,
+        "makingprocess": {
+            "ing1": {"name": "whiskey", "amount": 12},
+            "ing2": {"name": "sweet vermouth", "amount": 6},
+            "ing3": {"name": "bitters", "amount": 2}
+        }},
+
+        {"name": "hugo spritz",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "elderflower liqueur", "amount": 4},
+            "ing2": {"name": "prosecco", "amount": 10},
+            "ing3": {"name": "soda water", "amount": 4},
+            "ing4": {"name": "mint", "amount": 2},
+            "ing5": {"name": "ice", "amount": 1}
+        }},
+
+        {"name": "elderflower collins",
+        "price": 9,
+        "makingprocess": {
+            "ing1": {"name": "gin", "amount": 6},
+            "ing2": {"name": "elderflower liqueur", "amount": 4},
+            "ing3": {"name": "lemon juice", "amount": 4},
+            "ing4": {"name": "soda water", "amount": 6},
+            "ing5": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "mango daiquiri",
+        "price": 11,
+        "makingprocess": {
+            "ing1": {"name": "white rum", "amount": 8},
+            "ing2": {"name": "mango juice", "amount": 6},
+            "ing3": {"name": "lime juice", "amount": 4},
+            "ing4": {"name": "sugar syrup", "amount": 2}
+        }},
+
+        {"name": "passion fruit mojito",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "white rum", "amount": 5},
+            "ing2": {"name": "passion fruit juice", "amount": 4},
+            "ing3": {"name": "lime juice", "amount": 3},
+            "ing4": {"name": "sugar syrup", "amount": 2},
+            "ing5": {"name": "soda water", "amount": 4},
+            "ing6": {"name": "mint", "amount": 2},
+            "ing7": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "peach bellini royale",
+        "price": 12,
+        "makingprocess": {
+            "ing1": {"name": "peach schnapps", "amount": 4},
+            "ing2": {"name": "peach juice", "amount": 6},
+            "ing3": {"name": "prosecco", "amount": 10}
+        }},
+
+        {"name": "apple mule",
+        "price": 10,
+        "makingprocess": {
+            "ing1": {"name": "vodka", "amount": 6},
+            "ing2": {"name": "apple juice", "amount": 6},
+            "ing3": {"name": "ginger beer", "amount": 6},
+            "ing4": {"name": "lime juice", "amount": 2},
+            "ing5": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "tropical sunrise",
+        "price": 11,
+        "makingprocess": {
+            "ing1": {"name": "tequila", "amount": 6},
+            "ing2": {"name": "mango juice", "amount": 7},
+            "ing3": {"name": "passion fruit juice", "amount": 5},
+            "ing4": {"name": "grenadine", "amount": 2},
+            "ing5": {"name": "ice", "amount": 2}
+        }},
+
+        {"name": "green demon",
+        "price": 12,
+        "makingprocess": {
+            "ing1": {"name": "midori", "amount": 5},
+            "ing2": {"name": "vodka", "amount": 5},
+            "ing3": {"name": "pineapple juice", "amount": 8},
+            "ing4": {"name": "blue curaçao", "amount": 2},
+            "ing5": {"name": "ice", "amount": 2}
+        }}
+    ]
