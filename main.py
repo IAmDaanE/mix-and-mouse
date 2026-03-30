32c981a76d206f7444fd2e9383e3c044c2f28b73
Twan <twanVanhoutte@gmail.com>
2026-03-30 21:22:08 +0200

    cocktail made screen

--- main.py
+++ main.py
@@ -1625,7 +1625,7 @@ if True:
         screen.blit(error_text, (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
     def display_cocktail_made_screen():
-        screen.fill((255, 0, 255))
+        screen.blit(cocktail_stats_background_img, (213,120))
         test_text = pixel_font_letters.render(str(current_made_cocktail), True, (255, 255, 255))
         screen.blit(test_text, (200, 100))
 
