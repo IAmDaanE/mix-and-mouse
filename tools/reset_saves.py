8e5d93fb62e59610283d9610e4d9c2f1cb246560
Daan Eeckloo <daan.eeckloo@gmail.com>
2026-08-06 23:26:02 +0200

    fixed reset_saves file

--- tools/reset_saves.py
+++ tools/reset_saves.py
@@ -2,6 +2,6 @@ import os
 from platformdirs import user_data_dir
 import shutil
 
-save_files_location = user_data_dir('cocktail_game', 'DTstudios')
+save_files_location = user_data_dir('mix-and-mouse', 'DTstudios')
 
 shutil.rmtree(save_files_location)
